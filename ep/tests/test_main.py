fe6b55369b9707291fa00c26a76abe03fbe5e726
Carles Barrobés <carles@barrobes.com>
2015-04-10 14:55:12 +0000

    Add parsing tests

---
+++ ep/tests/test_main.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from ..main import EP
+
+
+class TestParsing(TestCase):
+
+    def test_yaml_parsing(self):
+        ep = EP('ep/samples/example.yml')
+        self.assertEqual(2, len(ep.env))
+        self.assertEqual(1, len(ep.dependencies))
+        self.assertEqual('echo "Hello World"', ep._run)
+
+    def test_yaml_parsing_defaults(self):
+        ep = EP('ep/samples/defaults.yml')
+        self.assertEqual(1, len(ep.env))
+        self.assertEqual(5555, ep.env[0].default)
+        self.assertEqual('A number between 5000 and 7000', ep.env[0].help)
